739c85a2bc270bfa7dab330498b405486a88f157
bendee <benbanj@hotmail.com>
2025-12-02 18:07:25 +0000

    Adding some default endpoints
    - Adding some endpoints
    - Adding the openai sdk

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ import express from 'express';
 import cors from 'cors';
 import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
+import OpenAI from 'openai';
 
 const app = express();
+const client = new OpenAI(); // connect to openai sdk
 
 // Security Middleware courtesy of Helmet makes things bare secure
 app.use(helmet())
@@ -22,3 +24,26 @@ const limiter = rateLimit({
   message: "Request limit hit, I'm not made of money, try again later."
 })
 app.use(limiter)
+
+// Endpoints
+
+// Default
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+
+app.get("/api/suggest-film", async (req, res) => {
+  try {
+    console.log('hello')
+    res.json({result: "hello"})
+  } catch(e) {
+    console.log(e.message)
+  }
+})
+
+// Connect express app
+const PORT = process.env.PORT || 3002
+app.listen(PORT, () => {
+  console.log(`API server listening on http://localhost:${PORT}`)
+})
