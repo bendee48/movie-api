b6b4989658f709f0bd195eae5b3e0af6f235c833
bendee <benbanj@hotmail.com>
2026-04-29 18:18:24 +0100

    Adding Streaming info
    - Adding helper function to return user's current geo region
    - Updating prompt to return film streaming info

--- server.js
+++ server.js
@@ -31,6 +31,15 @@ app.use(limiter)
 // Open AI SDK
 const client = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
 
+// Helper Functions
+
+function getCountry(req) {
+  return (
+    req.headers["cf-ipcountry"] ||
+    "UK"
+  );
+}
+
 //* Endpoints *//
 
 // Root
@@ -41,24 +50,31 @@ app.get("/", (req, res) => {
 // POST single random film suggestion
 app.post("/api/film/lucky", async (req, res) => {
   const { previousFilms = [] } = req.body;
+  const country = getCountry(req);
 
   try {
     const response = await client.responses.create({
       model: 'gpt-5-nano-2025-08-07',
       instructions: 
-        `Return a film name, the year of it\'s release, the director, a few of the main actors involved,
-         and a short summary of the film with no spoilers. 
-         The film can be from any genre, year, country etc but generally regarded as being good
-         by either critics or audiences.
+        `You are an experienced film critic. Return a film name, the year of it\'s release, the director, a few of the main actors involved,
+         a short summary of the film (with no spoilers) and a list of streaming services that are currently showing said film. 
+         The film can be from any genre, year, country etc but ensure it is generally regarded as being "good", predominantly by
+         film critics, then secondly by audiences. As a film critic aim to reccomend something good that might be lesser seen
+         by audiences.
+         The streaming information will be for the region ${country}. Show what the service is and provide a link to the film.
+         Do not return any of these films ${previousFilms.join(', ')}.
          Return JSON in this format:
           {
             "title": "",
             "year": "",
             "director": "",
             "stars": "",
-            "summary": ""
+            "summary": "",
+            "streaming": [
+              { "service": "", "url": "" }
+            ]
           }
-         Do not return any of these films ${previousFilms.join(', ')}.`,
+        `,
       input: 'Suggest a good film to watch.'
     })
     res.status(200).json({result: response.output_text});
@@ -72,26 +88,32 @@ app.post("/api/film/lucky", async (req, res) => {
 app.post("/api/film", async (req, res) => {
   const { genre, decade, runtime, rating, language } = req.query;
   const { previousFilms = [] } = req.body;
+  const country = getCountry(req);
   
   try {
     const response = await client.responses.create({
       model: 'gpt-5-nano-2025-08-07',
       instructions: 
-        `Return a film name, the year of it\'s release, the director, a couple of the main actors involved
-         and a short summary of the film with no spoilers.
+        `You are an experienced film critic. Return a film name, the year of it\'s release, the director, a few of the main actors involved,
+         a short summary of the film (with no spoilers) and a list of streaming services that are currently showing said film.
+         The streaming information will be for the region ${country}. Show what the service is and provide a link to the film.
+         Do not return any of these films ${previousFilms.join(', ')}.
          Return JSON in this format:
           {
             "title": "",
             "year": "",
             "director": "",
             "stars": "",
-            "summary": ""
+            "summary": "",
+            "streaming": [
+              { "service": "", "url": "" }
+            ]
           }
          Or if you're unable to find a suitable film within those parameters return JSON in this format:
           {
             "notFound": "true"
           }
-         Do not return any of these films ${previousFilms.join(', ')}.`,
+        `,
       input: 
         `Suggest a film to watch that is of the genre '${genre},' was released in ${decade},
          has a runtime of ${runtime}, is in ${language} and has an imdb rating of ${rating}.
